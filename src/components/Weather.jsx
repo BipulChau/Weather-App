d788360cd1360a6d3f1df7067292548b7fcf252d
Bipul Chaudhary <bipulchau@gmail.com>
2021-11-15 00:57:43 -0500

    Created elements in Weather componnent and also added styling in index.css

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,12 +1,23 @@
 import { Fragment } from "react";
-import "./weather.css"
+import "./weather.css";
 
 const Weather = () => {
   return (
-  <div className="weather">
-    <span className = "condition">30 C</span>
-    <img className= "logo" src="/logo/perfect-day.svg" alt="perfect-day logo"/>
-    </div>);
+    <Fragment>
+      <span className="react_app">Weather App</span>
+    <div className="weather">
+      <span className="condition">30 C</span>
+      <span className="cloudy">| Cloudy</span>
+      <img
+        className="logo"
+        src="/logo/perfect-day.svg"
+        alt="perfect-day logo"
+      />  
+    </div>
+    <span className="location">Toronto, CA</span>
+    <span className="weather_info">Weather Info</span>
+    </Fragment>
+  );
 };
 
 export default Weather;
