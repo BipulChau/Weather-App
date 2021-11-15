04e391c32ff891f724e2c27dce0fa33b3275e04d
Bipul Chaudhary <bipulchau@gmail.com>
2021-11-14 22:56:27 -0500

    Label created and provided styling to it

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,7 +1,8 @@
 import"./city.css"
 const City = () => {
   return <div className="city">
-    <img className= "logo" src="/logo/perfect-day.svg"/>
+    <img className= "logo" src="/logo/perfect-day.svg" alt="perfect-day logo"/>
+    <span className="choose_city">Check Weather of any city</span>
     </div>;
 };
 
