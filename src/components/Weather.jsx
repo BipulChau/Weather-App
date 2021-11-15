5e3bc14989c716b138b0192bb5433a3d67939cdc
Bipul Chaudhary <bipulchau@gmail.com>
2021-11-15 01:15:07 -0500

    Create Weatherinfo component and css file for it

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,10 +1,11 @@
 import { Fragment } from "react";
 import "./weather.css";
+import WeatherInfo from "./WeatherInfo";
 
 const Weather = () => {
   return (
     <Fragment>
-      <span className="react_app">Weather App</span>
+      {/* <span className="react_app">Weather App</span> */}
     <div className="weather">
       <span className="condition">30 C</span>
       <span className="cloudy">| Cloudy</span>
@@ -16,6 +17,7 @@ const Weather = () => {
     </div>
     <span className="location">Toronto, CA</span>
     <span className="weather_info">Weather Info</span>
+    <WeatherInfo/>
     </Fragment>
   );
 };
---
+++ src/components/WeatherInfo.jsx
@@ -0,0 +1,14 @@
+import "./weatherinfo.css";
+
+const WeatherInfo = (props) => {
+  return (
+    <div className="weather_details">
+      <span>Tepra</span>
+      <span>Kapala</span>
+      <span>Gopu</span>
+      <span>Mendak</span>
+    </div>
+  )
+}
+
+export default WeatherInfo
