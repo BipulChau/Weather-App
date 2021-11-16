3481236a6e84fef4c36d588ef20bf51ccf7dd66e
Bipul Chaudhary <bipulchau@gmail.com>
2021-11-16 00:07:28 -0500

    It will change unix timestamp to normal time. Added app images and also readme

--- src/components/WeatherInfoItems.jsx
+++ src/components/WeatherInfoItems.jsx
@@ -4,18 +4,30 @@ import WeatherInfoItem from "./WeatherInfoItem";
 const WeatherInfoItems = (props) => {
   const { weather } = props;
   const isNight = weather?.weather[0].icon?.includes("n");
-  
-//   const timeStampToTime = (unixTimestamp) => {
-//    const time = moment(unixTimestamp).format("DD-MM-YYYY h:mm:ss")
-//     return time
-//   };
-// console.log(timeStampToTime(weather.sys[isNight?"sunrise":"sunset"]))
+
+  const timeStampToTime = (unixTimestamp) => {
+    // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+    const date = new Date(unixTimestamp * 1000);
+    // Hours part from the timestamp
+    var hours = date.getHours();
+    // Minutes part from the timestamp
+    var minutes = "0" + date.getMinutes();
+    // Seconds part from the timestamp
+    var seconds = "0" + date.getSeconds();
+
+    // Will display time in 10:30:23 format
+    var formattedTime =
+      hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
+
+    return formattedTime;
+  };
+  console.log(timeStampToTime(weather.sys[isNight ? "sunrise" : "sunset"]));
 
   return (
     <div className="weather_details">
       <WeatherInfoItem
         name={isNight ? "sunrise" : "sunset"}
-        value={weather.sys[isNight ? "sunrise" : "sunset"]}
+        value={timeStampToTime(weather.sys[isNight ? "sunrise" : "sunset"])}
       />
       <WeatherInfoItem name="humidity" value={weather?.main?.humidity} />
       <WeatherInfoItem name="wind" value={weather?.wind?.speed} />
